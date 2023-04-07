288c36daf78792fa3857e0612e74b4331c3246e9
Tony Chau <90050474+fo5for@users.noreply.github.com>
2023-04-07 21:18:40 +0800

    [core] Suppress printing to stdout when writer attached

--- chat_downloader/sites/common.py
+++ chat_downloader/sites/common.py
@@ -301,7 +301,8 @@ class Chat():
         :param item: The chat item to be printed
         :type item: dict
         """
-        safe_print(self.format(item), flush=flush)
+        if self._output_writer is None:
+            safe_print(self.format(item), flush=flush)
 
     def format(self, item):
         """Format chat messages
